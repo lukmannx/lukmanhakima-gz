37d6930f9297d0f9137d9d5c363de229efd56c44
Lukman Hakim A <89436199+lukmannx@users.noreply.github.com>
2023-03-18 14:59:03 +0700

    Add files via upload

---
+++ MyApplication/app/src/main/java/com/example/myapplication/data/NabiData.kt
@@ -0,0 +1,66 @@
+package com.example.myapplication.data
+
+object NabiData {
+    val nabiList = arrayOf(
+        "Nabi Adam AS",
+        "Nabi Idris AS",
+        "Nabi Nuh AS",
+        "Nabi Hud AS",
+        "Nabi Sholeh AS",
+        "Nabi Ibrahim AS",
+        "Nabi Luth AS",
+        "Nabi Ismail AS",
+        "Nabi Ishaq AS",
+        "Nabi Ya'qub AS",
+        "Nabi Yusuf AS",
+        "Nabi Ayyub AS",
+        "Nabi Syu'aib AS",
+        "Nabi Musa AS",
+        "Nabi Harun AS",
+        "Nabi Dzulkifli AS",
+        "Nabi Dawud AS",
+        "Nabi Sulaiman AS",
+        "Nabi Ilyas AS",
+        "Nabi Ilyasa' AS",
+        "Nabi Yunus AS",
+        "Nabi Zakariya AS",
+        "Nabi Yahya AS",
+        "Nabi Isa' AS",
+        "Nabi Muhammad SAW"
+    )
+
+    val tahunList = arrayOf(
+        "5876",
+        "4533",
+        "3993",
+        "2450",
+        "2450",
+        "1997",
+        "1897",
+        "1911",
+        "1897",
+        "1837",
+        "1745",
+        "1600",
+        "1527",
+        "1531",
+        "1500",
+        "1063",
+        "989",
+        "910",
+        "885",
+        "820",
+        "91",
+        "1",
+        "1",
+        "570"
+    )
+
+    val usiaList = arrayOf(
+        "950",
+        "345",
+        "950",
+        "130",
+        ""
+    )
+}
